f397b01f4ad1540dac676dd3440fb7b417ecfabc
joachim Bourquardez <j.bourquardez@laposte.net>
2020-01-27 21:27:35 +0000

    qs reglages dans environnement production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ Rails.application.configure do
 
   config.hosts << "meinmarseille.herokuapp.com"
 
-  config.action_mailer.default_url_options = { host: "http://TODO_PUT_YOUR_DOMAIN_HERE" }
+  config.action_mailer.default_url_options = { host: "meinmarseille.herokuapp.com" }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
