9d3796a424c2cb3a9d4b36714980f6163b566f4d
joachim Bourquardez <j.bourquardez@laposte.net>
2020-01-27 21:35:54 +0000

    desctivation du mail a l'inscription, pour voir si le site fonctionne comme ca sur heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,8 @@ after_create :send_welcome_email
 
   private
 
-  def send_welcome_email
-    UserMailer.welcome(self).deliver_now
-  end
+  #def send_welcome_email
+   # UserMailer.welcome(self).deliver_now
+  #end
 
 end
