02a55c30afb43c47f587bb35fc686855e7f0a39c
joachim Bourquardez <j.bourquardez@laposte.net>
2020-01-29 13:43:21 +0000

    essaie mail avec mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,6 +6,14 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: "meinmarseille.herokuapp.com" }
   # Settings specified here will take precedence over those in config/application.rb.
   config.action_mailer.delivery_method = :smtp
+   config.action_mailer.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'meinmarseille.herokuapp.com', #eg: 'yourappname.herokuapp.com'
+    :authentication => :plain,
+  }
   # Code is not reloaded between requests.
   config.cache_classes = true
 
