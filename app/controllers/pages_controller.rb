bf6b68b878e4aa65ae68907d608178cecea75921
joachim Bourquardez <j.bourquardez@laposte.net>
2020-02-11 21:03:58 +0000

    repositionnement du ht access

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:home, :cgu, :contact, :equipe, :faq, :prix, :projet, :user]
 
 
   def home
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,6 @@
 class ProductsController < ApplicationController
+skip_before_action :authenticate_user!, only: [:show, :index]
+
   def index
     @les_produits = Product.page(params[:page]).per(8)
   end
